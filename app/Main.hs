db670212545ec93e51bed0435c69354ad8ecc173
Kevin Cao <kjcao@proton.me>
2024-05-29 23:37:54 -0400

    Add cli arguments

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,35 @@
 
 module Main where
 
-import Network.Wreq
 import Control.Lens
+import Network.Wreq hiding (header)
+import Options.Applicative
+
+data Arguments = Arguments
+  {url :: String}
+
+parseArgs :: Parser Arguments
+parseArgs =
+  Arguments
+    <$> strOption
+      ( long "url"
+          <> short 'u'
+          <> metavar "URL"
+          <> help "URL of website to save"
+      )
 
 main :: IO ()
-main = do
-  r <- get "http://example.org"
+main = run =<< execParser opts
+  where
+    opts =
+      info
+        (parseArgs <**> helper)
+        ( fullDesc
+            <> progDesc "Save/mirror the URL website"
+            <> header "mirror a website"
+        )
+
+run :: Arguments -> IO ()
+run (Arguments url) = do
+  r <- get url
   print $ r ^. responseBody
