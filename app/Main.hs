8069a46496d5cffbbec478973324a0f36a22d8ce
Kevin Cao <kjcao@proton.me>
2024-06-01 12:41:00 -0400

    Add percent encoding

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,13 @@ import System.Directory
 import System.FilePath.Posix (takeDirectory)
 import qualified Data.ByteString.Lazy as B
 import qualified Url
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Html
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TI
+import Data.Char (isAlphaNum, toUpper)
+import Numeric (showHex)
 
 data Arguments = Arguments
   {saveUrl :: String}
@@ -47,16 +50,34 @@ saveAsset oldUrl path = do
     url = Url.orient oldUrl path
     savePath = if head path == '/' then tail path else path
 
-attrValue :: Html.Attr -> T.Text
+attrValue :: Html.Attr -> Text
 attrValue (Html.Attr _ v) = v
 
-isValidUrl :: T.Text -> Bool
+isValidUrl :: Text -> Bool
 isValidUrl url =
   case Url.parseUrl (T.unpack url) of
     Right _ -> True
     Left  _ -> False
 
-saveAssets :: Url.Url -> T.Text -> IO T.Text
+isSrcAttribute :: Html.Attr -> Bool
+isSrcAttribute (Html.Attr "src" _) = True
+isSrcAttribute (Html.Attr "data-src" _) = True
+isSrcAttribute _ = False
+
+isHrefAttribute :: Html.Attr -> Bool
+isHrefAttribute (Html.Attr "href" _) = True
+isHrefAttribute _ = False
+
+percentEncode :: Text -> Text
+percentEncode =
+  T.concatMap
+    (\c ->
+      if isAlphaNum c || c == '/' -- DO NOT percent encode "/" because that's used in file path
+        then T.pack [c]
+        else T.pack $ '%' : (toUpper <$> showHex (fromEnum c) "")
+    )
+
+saveAssets :: Url.Url -> Text -> IO Text
 saveAssets url src = do
   let toks = Html.parseTokens src
   let assets' = concatMap (filter (/="") . filter (not . T.isPrefixOf "data:") . match . snd) toks
@@ -66,25 +87,19 @@ saveAssets url src = do
   let newSrc = map adjust toks
   return (Html.recoverTokens newSrc)
   where
-
-    isSrcAttribute (Html.Attr "src" _) = True
-    isSrcAttribute (Html.Attr "data-src" _) = True
-    isSrcAttribute _ = False
-
-    isHrefAttribute (Html.Attr "href" _) = True
-    isHrefAttribute _ = False
-    
     match (Html.TagSelfClose "img" a) = map attrValue $ filter isSrcAttribute a
     match (Html.TagOpen "img" a) = map attrValue $ filter isSrcAttribute a
     match (Html.TagSelfClose "link" a) = map attrValue $ filter isHrefAttribute a
     match (Html.TagOpen "link" a) = map attrValue $ filter isHrefAttribute a
     match _ = []
 
+    g "" = ""
     g p = if T.head p == '/' then absolute p else p
       where absolute path = T.pack (concat $ replicate (length $ filter (=='/') $ Url.path url) "../") <> T.tail path
 
+    -- For some reason this works. Please help.
     adjustAttrs :: (Html.Attr -> Bool) -> [Html.Attr] -> [Html.Attr]
-    adjustAttrs f = mapIf f (\(Html.Attr k v) -> Html.Attr k (g v))
+    adjustAttrs p = mapIf p (\(Html.Attr k v) -> Html.Attr k (percentEncode v))
       where mapIf p f = map (\x -> if p x then f x else x)
 
     adjust (_, Html.TagSelfClose "img" attrs) =
