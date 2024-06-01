2e8a79edbf97af8c55720192bec85a543a05272d
Kevin Cao <kjcao@proton.me>
2024-06-01 12:00:00 -0400

    Add ability to save <link href="...">

--- app/Main.hs
+++ app/Main.hs
@@ -15,13 +15,13 @@ import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TI
 
 data Arguments = Arguments
-  {url :: String}
+  {saveUrl :: String}
 
 parseArgs :: Parser Arguments
 parseArgs =
   Arguments
     <$> strOption
-      ( long "url"
+      ( long "saveUrl"
           <> short 'u'
           <> metavar "Url"
           <> help "Url of website to save"
@@ -39,7 +39,7 @@ main = run =<< execParser opts
         )
 
 saveAsset oldUrl path = do
-  putStrLn $ "saving asset " <> (Url.toString url)
+  putStrLn $ "saving asset " <> Url.toString url
   createDirectoryIfMissing True $ takeDirectory savePath
   r <- get $ Url.toString url
   B.writeFile savePath (r ^. responseBody)
@@ -47,28 +47,53 @@ saveAsset oldUrl path = do
     url = Url.orient oldUrl path
     savePath = if head path == '/' then tail path else path
 
-mapIf p f = map (\x -> if p x then f x else x)
+attrValue :: Html.Attr -> T.Text
+attrValue (Html.Attr _ v) = v
+
+isValidUrl :: T.Text -> Bool
+isValidUrl url =
+  case Url.parseUrl (T.unpack url) of
+    Right v -> True
+    Left  e -> False
 
 saveAssets :: Url.Url -> T.Text -> IO T.Text
 saveAssets url src = do
   let toks = Html.parseTokens src
-  let assets = concat $ map ((\(Html.TagSelfClose _ a) -> map (\(Html.Attr _ v) -> v) $ filter isSrcAttribute a)) (filter isImg (map snd toks))
-  let paths = filter (/="") $ filter (not . T.isPrefixOf "data:") assets
-  -- print $ paths
-  mapM_ (saveAsset url . T.unpack) paths
-  let test = mapIf (isImg . snd) (\(_, (Html.TagSelfClose _ attrs)) -> let new = Html.TagSelfClose "img" (adjust attrs) in (Html.renderToken new, new)) toks
-  print $ test
-  return (Html.recoverTokens $ test)
-  where
-    isSrcAttribute (Html.Attr "src" value) = True
-    isSrcAttribute (Html.Attr "data-src" value) = True
-    isSrcAttribute _ = False
- 
-    adjust :: [Html.Attr] -> [Html.Attr]
-    adjust = mapIf isSrcAttribute (\(Html.Attr k v) -> (Html.Attr k (if T.head v == '/' then (T.pack $ concat $ replicate (length $ filter (=='/') $ Url.path url) "../") <> T.tail v else v)))
+  let assets' = concatMap (filter (/="") . filter (not . T.isPrefixOf "data:") . match . snd) toks
+  let assets = filter (not . isValidUrl) assets' -- TODO add support to download external URLs
+  mapM_ (saveAsset url . T.unpack) assets
 
-    isImg (Html.TagSelfClose "img" _) = True
-    isImg _ = False
+  let newSrc = map adjust toks
+  return (Html.recoverTokens newSrc)
+  where
+
+    isSrcAttribute (Html.Attr "src" _) = True
+    isSrcAttribute (Html.Attr "data-src" _) = True
+    isSrcAttribute _ = False
+
+    isHrefAttribute (Html.Attr "href" _) = True
+    isHrefAttribute _ = False
+    
+    match (Html.TagSelfClose "img" a) = map attrValue $ filter isSrcAttribute a
+    match (Html.TagOpen "img" a) = map attrValue $ filter isSrcAttribute a
+    match (Html.TagSelfClose "link" a) = map attrValue $ filter isHrefAttribute a
+    match (Html.TagOpen "link" a) = map attrValue $ filter isHrefAttribute a
+    match _ = []
+
+    g p = if T.head p == '/' then absolute p else p
+      where absolute path = T.pack (concat $ replicate (length $ filter (=='/') $ Url.path url) "../") <> T.tail path
+
+    adjustAttrs :: (Html.Attr -> Bool) -> [Html.Attr] -> [Html.Attr]
+    adjustAttrs f = mapIf f (\(Html.Attr k v) -> Html.Attr k (g v))
+      where mapIf p f = map (\x -> if p x then f x else x)
+
+    adjust (_, Html.TagSelfClose "img" attrs) =
+      let new = Html.TagSelfClose "img" (adjustAttrs isSrcAttribute attrs)
+      in (Html.renderToken new, new)
+    adjust (_, Html.TagOpen "link" attrs) =
+      let new = Html.TagOpen "link" (adjustAttrs isHrefAttribute attrs)
+      in (Html.renderToken new, new)
+    adjust x = x
 
 save :: Url.Url -> IO ()
 save url = do
